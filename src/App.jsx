3893147da253730f36d5008c2b3989cdfba4836f
M4theusRibeiro <matheusribeiro3105@gmail.com>
2023-09-25 17:20:01 -0300

    Estrutura vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // A URL da API que você deseja acessar
-    const apiUrl = 'http://localhost:8080/usuario';
-
-    // Faz a solicitação GET usando Axios
-    axios.get(apiUrl)
-      .then((response) => {
-        setData(response.data); // Define os dados recebidos no estado
-        setLoading(false); // Define o estado de carregamento como falso
-      })
-      .catch((error) => {
-        console.error('Erro na solicitação:', error);
-      });
-  }, []);
-
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Dados da API:</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
